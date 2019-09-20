4d80e7210b0f5cac57878095ad1737d66d0e24ec
aayushi2919 <aayushi2919@gmail.com>
2019-09-20 14:54:33 +0530

    Add files via upload

---
+++ build_model.py
@@ -0,0 +1,37 @@
+from model import NLPModel
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def build_model():
+    model = NLPModel()
+
+
+    data = pd.read_csv('extract_combined.csv')
+    data2 = pd.read_csv('labels.csv', error_bad_lines = False)
+    merged= pd.merge(data,data2)
+    yn = {'Yes': 1,'No': 0} 
+   
+    merged.is_fitara = [yn[i] for i in merged.is_fitara] 
+
+
+    model.vectorizer_fit(data.loc[:, 'text'])
+    print('Vectorizer fit complete')
+
+    X = model.vectorizer_transform(data.loc[:, 'text'])
+    print('Vectorizer transform complete')
+    y = merged.loc[:, 'is_fitara']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model.train(X_train, y_train)
+    print('Model training complete')
+
+    model.pickle_clf()
+    model.pickle_vectorizer()
+
+    #model.plot_roc(X_test, y_test)
+
+
+if __name__ == "__main__":
+    build_model()
